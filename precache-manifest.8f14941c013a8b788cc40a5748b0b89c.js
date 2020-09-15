c7b5e3d0f6ab778d45da1c45d5609e3068f936b1
Jayraj Shah <jayrajcshah@gmail.com>
2020-09-16 02:00:30 +0530

    Updates

---
+++ precache-manifest.8f14941c013a8b788cc40a5748b0b89c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc540da3c217fe275498c1bf2112031f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ddd7012d8382ba55d67",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "84d54c40cd4f9bbcd357",
+    "url": "/static/css/main.19c70d24.chunk.css"
+  },
+  {
+    "revision": "3ddd7012d8382ba55d67",
+    "url": "/static/js/2.ae974ba4.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/static/js/2.ae974ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d54c40cd4f9bbcd357",
+    "url": "/static/js/main.456e3ebc.chunk.js"
+  },
+  {
+    "revision": "75e2c70bcf72ef5b56a5",
+    "url": "/static/js/runtime-main.91c961cd.js"
+  },
+  {
+    "revision": "3b5635ac5d39c28200bf3993d90d09c7",
+    "url": "/static/media/Raleway-VariableFont_wght.3b5635ac.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3c10c068e4e65938930e9e216309d03.js"
+  "/precache-manifest.8f14941c013a8b788cc40a5748b0b89c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
